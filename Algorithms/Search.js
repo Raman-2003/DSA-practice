9c93db07f96d82522b738db1200c5bb39facc9f9
Raman-2003 <ramankishore838@gmail.com>
2024-07-26 10:59:18 +0530

    un commands

--- Algorithms/Search.js
+++ Algorithms/Search.js
@@ -40,24 +40,24 @@ console.log(binarySearch([-5, 2, 4, 6, 10], 10));
 console.log(binarySearch([-5, 2, 4, 6, 10], 20));
 
 
-// function recursiveBinary(array, target){
-//     return binarySearch(array, target, 0, array.length-1)
-// }
+function recursiveBinary(array, target){
+    return binarySearch(array, target, 0, array.length-1)
+}
 
-// function search(array, target, leftIndex, rightIndex){
-//         if(leftIndex > rightIndex){
-//             return null;
-//         }
-//         let middleIndex = Math.floor((leftIndex+rightIndex)/2);
-//         if(target === array[middleIndex]){
-//             return middleIndex
-//         }
-//         if(target < array[middleIndex]){
-//             return search(array, target, leftIndex, middleIndex-1)
-//         }else{
-//             return search(array, target, middleIndex+1, rightIndex)
-//         }
-// }
+function search(array, target, leftIndex, rightIndex){
+        if(leftIndex > rightIndex){
+            return null;
+        }
+        let middleIndex = Math.floor((leftIndex+rightIndex)/2);
+        if(target === array[middleIndex]){
+            return middleIndex
+        }
+        if(target < array[middleIndex]){
+            return search(array, target, leftIndex, middleIndex-1)
+        }else{
+            return search(array, target, middleIndex+1, rightIndex)
+        }
+}
 
 
 
