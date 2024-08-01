288007caf2ccdda939508a6a3b866dda2127f614
Raman-2003 <ramankishore838@gmail.com>
2024-08-02 00:59:10 +0530

    image terminlogy

---
+++ 14/tree.js
@@ -0,0 +1,33 @@
+/*
+
+1. This is a kind of Custom Data Structure in JavaScript
+2. A tree is a hierarchial data structure that consits of nodes connected by edges. Each node stores a data value.
+3. Tree is a Non linear data structure. Stacks, queues, Linked list and arrays are Linear data structures.
+4. In linear DS, the time required to search is proportional to the size of the data set. Which means, the input size is grows, It takes time based uopn the input size. 
+   More the data, more the time needed to search
+5. A tree will not contain any loops pr cycles and But it is efficient and easier access to the data.
+
+
+Tree Visualization
+We have a node and their respective values. Trees are connected with multiple nodes and edges create a hierarchical visuals.
+
+
+Apllications:
+1. File systems for DIRECTORY structure
+2. A Familt tree
+3. An Organization tree
+4. DOM and ChatBots
+
+
+Tree terminolgy:
+1. Basically => Parent node and child node. A is the parent and other connectors are child successors.
+2. Root node => the node tree originates is called root node. "Base node". It does not have Parent node. A is the root node
+3. Leaf nodes => which are nodes that do not have child nodes. 
+4. The Degree of a node is the total numbers of child nodoes it has.
+5. Depth of root node is always 0. Depth of E is 2.   . A <= B <= E
+6. Height of B => 1. Height of A => 2
+7. Nodes with same parent are called Siblings.
+
+Refer all images in Terminologies
+
+*/
